405712a35c11b1337e0ec54432bde9b1d201ce45
Harsh Chovatiya <harshchovatiya16@gmail.com>
2024-08-20 20:27:08 +0530

    second commit

--- script.js
+++ script.js
@@ -11,20 +11,27 @@ function firstPageAnim() {
     var tl = gsap.timeline();
   
     tl.from("#nav", {
-      y: -10,
+      x: 100,
       opacity: 0,
       duration: 1.5,
       ease: Expo.easeInOut,
     })
       .to(".anim", {
-        y: 0,
+        x: 0,
         ease: Expo.easeInOut,
         duration: 2,
         delay: -1,
         stagger: 0.2,
       })
+      .to(".anim1", {
+        x: 0,
+        ease: Expo.easeInOut,
+        duration: 2,
+        delay: -1,
+        stagger: 0.1,
+      })
       .from("#herofooter", {
-        y: "-10",
+        x: 100,
         opacity: 0,
         duration: 1.5,
         delay: -1,
@@ -33,49 +40,17 @@ function firstPageAnim() {
     }
 // --------------------------------------------------------------
 
-// mouse round scale ------------------------------------------------
-
-var timeout;
-
-function mousescale() {
-  // define default scale value
-  var xscale = 1;
-  var yscale = 1;
-
-  var xprev = 0;
-  var yprev = 0;
-
-  window.addEventListener("mousemove", function (dets) {
-    clearTimeout(timeout);
-
-    xscale = gsap.utils.clamp(0.8, 1.2, dets.clientX - xprev);
-    yscale = gsap.utils.clamp(0.8, 1.2, dets.clientY - yprev);
-
-    xprev = dets.clientX;
-    yprev = dets.clientY;
-
-    shery(xscale, yscale);
-
-    timeout = setTimeout(function () {
-      document.querySelector("#minicircle").style.transform = `translate(${dets.clientX}px, ${dets.clientY}px) scale(1, 1)`;
-    },100);
-  });
-}
-
-// ----------------------------------------------------------------------
-
-// mouse roundmate -------------------------------------------------
-function shery(xscale, yscale){
-    window.addEventListener("mousemove",function(dets){
-        document.querySelector("#minicircle").style.transform = `translate(${dets.clientX}px, ${dets.clientY}px) scale(${xscale}, ${yscale})`;
-    })
-}
-
-mousescale()
-shery()
 firstPageAnim()
 
+// mouse roundmate -------------------------------------------------
+Shery.mouseFollower({
+ 
+});
 
+
+Shery.makeMagnet("#a" , {
+  
+});
 
 
 document.querySelectorAll(".elem").forEach(function (elem) {
